ec55587d256674e51511e076699a3cefde969c39
Daniel <schechterdani@gmail.com>
2019-08-10 17:23:55 +0300

    UI changes

--- app/src/main/java/com/example/profily/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/profily/Profile/ProfileFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -40,7 +41,7 @@ public class ProfileFragment extends Fragment {
     private TextView profileUsername,profileDescription;
     private ImageView profileImage;
     private Button editProfileBtn;
-
+    private ImageButton logoutButton;
 
     //counting vars
     private TextView profileNumOfFollowers;
@@ -71,6 +72,7 @@ public class ProfileFragment extends Fragment {
         profileNumOfPosts = view.findViewById(R.id.profile_posts_count);
         profileImage = view.findViewById(R.id.profile_image);
         editProfileBtn = view.findViewById(R.id.profile_edit_profile_btn);
+        logoutButton = view.findViewById(R.id.profile_logout);
 
         for(int i=0; i<6; i++)
         {
